c489d008ca181ca9427c82316e98adf973a6e92e
NAJMULHASAN001 <najmulhasan2001@gmail.com>
2026-01-16 13:13:37 +0600

    added dynamic time

--- home.js
+++ home.js
@@ -1,17 +1,72 @@
 console.log('js file connected')
 
-// function getElement(id){
-//     const element =document.getElementById(id);
-//     return element;
-// }
+function getElement(id){
+    const element =document.getElementById(id);
+    return element;
+}
+function getClass(className){
+    const element = document.getElementsByClassName(className);
+    return element;
+}
 
 
-const heartIcons =document.getElementsByClassName('heart-icon')
+const heartIcons = getClass('heart-icon')
 for(let heartIcon of heartIcons){
     heartIcon.addEventListener('click', function(){
         const heartCount =document.getElementById('heart-count').innerText;
         const currentCount = Number(heartCount) + 1;
         document.getElementById('heart-count').innerText = currentCount;
 
+    })
+}
+
+const callButtons = getClass('call-btn')
+for(let callButton of callButtons){
+    callButton.addEventListener("click", function(){
+        const getTitle = callButton.parentNode.parentNode.children[0].innerText
+        const getNum = callButton.parentNode.parentNode.children[2].innerText
+        const getCoinNum =Number(getElement('coin-count').innerText);
+        //-----------------------------------------------------
+        const getHistory = getElement('history-container')
+        const newHistory =document.createElement('div')
+        const time =new Date().toLocaleTimeString()
+       
+
+        if(getCoinNum >=20){
+            const currentCoin = getCoinNum - 20;
+            getElement('coin-count').innerText = currentCoin;
+            alert(`📞calling ${getTitle+' '+ getNum}`)
+            //add history--------------
+             newHistory.innerHTML =`
+        <div class="bg-[#f3f0f0] rounded-xl shadow-md p-2 flex justify-between mb-3">
+                    <div class="w-2/3">
+                        <p class="font-bold">${getTitle}</p>
+                        <p>${getNum}</p>
+                    </div>
+                    <div class="my-auto">
+                        <p>${time}</p>
+                    </div>
+
+                </div>
+        `
+        getHistory.append(newHistory)
+
+        }
+        else{
+            alert("❌You don't have sufficient coin")
+            return;
+        }
+
+
+
+
+
+
+
+
+
+        // console.log(getTitle, getNum, getCoinNum)
+
+        
     })
 }
